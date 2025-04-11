3e4d438165bbdb5a4728fb2db7e35cd6a0cb4be5
Nathaniel Dafoe <ndafoe60@gmail.com>
2025-04-11 09:06:56 -0600

    MAYBE ITS FIXED, MAYBE ITS NOT

--- js/load_header_and_footer.js
+++ js/load_header_and_footer.js
@@ -1,16 +1,17 @@
 // track control key state with a boolean
 const CONTROL_KEY_ = 'a';
-if(typeof(localStorage.getItem("username"))=='undefined'){
-    sessionStorage.setItem("controlCharDown", "false");
+
+if(localStorage.getItem("controlCharDown")==null){
+    sessionStorage.setItem("controlCharDown", "false")
 }
 
 document.addEventListener('keydown', e =>{
-    if (e.key == CONTROL_KEY_){
+    if (e.key.toLowerCase() == CONTROL_KEY_){
         sessionStorage.setItem("controlCharDown", "true");
     }
 });
 document.addEventListener('keyup', e =>{
-    if (e.key == CONTROL_KEY_){
+    if (e.key.toLowerCase() == CONTROL_KEY_){
         sessionStorage.setItem("controlCharDown", "false");
     }
 });
